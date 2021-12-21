4ee1e611a4b739cac68e68ced392d5e68e45fb92
luca <luca.bondesan02@gmail.com>
2021-12-21 12:50:04 +0100

    visualizzazzione own ip addres

--- Forza4/Forza4/MainWindow.xaml.cs
+++ Forza4/Forza4/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Net.NetworkInformation;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
@@ -25,19 +26,48 @@ namespace Forza4
         public MainWindow()
         {
             InitializeComponent();
-            txtip.Text = GetLocalIPAddress();
-        }
-        public static string GetLocalIPAddress()
-        {
-            var host = Dns.GetHostEntry(Dns.GetHostName());
-            foreach (var ip in host.AddressList)
+            string[] ips = test();
+            foreach (var item in ips)
             {
-                if (ip.AddressFamily == AddressFamily.InterNetwork)
-                {
-                    return ip.ToString();
-                }
+                txtip.Text += item + "\r\n";
             }
-            throw new Exception("No network adapters with an IPv4 address in the system!");
+            
+        }
+
+
+        public static string[] test()
+        {
+            try
+            {
+                string localIP;
+                using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp))
+                {
+                    socket.Connect("8.8.8.8", 65530);
+                    IPEndPoint endPoint = socket.LocalEndPoint as IPEndPoint;
+                    localIP = endPoint.Address.ToString();
+                }
+                return new string[] { localIP};
+            }
+            catch(Exception e)
+            {
+                NetworkInterfaceType _type = NetworkInterfaceType.Ethernet;
+                List<String> indirizzi = new List<string>();
+                foreach (NetworkInterface item in NetworkInterface.GetAllNetworkInterfaces())
+                {
+                    if (item.NetworkInterfaceType == _type && item.OperationalStatus == OperationalStatus.Up)
+                    {
+                        foreach (UnicastIPAddressInformation ip in item.GetIPProperties().UnicastAddresses)
+                        {
+                            if (ip.Address.AddressFamily == AddressFamily.InterNetwork)
+                            {
+                                indirizzi.Add(ip.Address.ToString());
+                            }
+                        }
+                    }
+                }
+                return indirizzi.ToArray<string>();
+            }
+            
         }
     }
 }
